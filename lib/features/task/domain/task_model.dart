2c2b7e3cbf418c837be5f80209830cb7b0531034
Octaviano <octaviano1604@gmail.com>
2025-12-04 04:17:10 +0800

    feat:devise structure Domain Layer

---
+++ lib/features/task/domain/task_model.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sync_task_app/features/task/domain/task_status.dart';
+
+class TaskModel {
+  final String id;
+  final String projectId;
+  final String title;
+  final String description;
+  final TaskStatus status;
+  final String assignedToId;
+  final String? proofUrl;
+  final DateTime dueDate;
+  final DateTime createAt;
+
+  TaskModel({
+    required this.id,
+    required this.projectId,
+    required this.title,
+    required this.description,
+    required this.status,
+    this.proofUrl,
+    required this.assignedToId,
+    required this.dueDate,
+    required this.createAt,
+  });
+
+  factory TaskModel.fromMap(Map<String, dynamic>map,String id){
+    return TaskModel(
+      id:id,
+      projectId: map['projectId'] as String,
+      title:map['title'] as String,
+      description:map['description'] as String,
+      status:TaskStatus.values.firstWhere(
+        (e)=>e.name == map['status'],
+        orElse:()=> TaskStatus.todo,
+      ),
+      proofUrl: map['proofUrl'] as String?,
+      assignedToId: map['assignedToId'] as String,
+      dueDate:map['dueDate'] as DateTime,
+      createAt: map['createAt'] as DateTime
+    );
+  }
+
+  Map<String,dynamic> toMap(){
+    return{
+      'projectId':projectId,
+      'title':title,
+      'description':description,
+      'proofUrl':proofUrl,
+      'assignedToId':assignedToId,
+      'status':status.name,
+      'dueDate':Timestamp.fromDate(dueDate),
+      'createAt':Timestamp.fromDate(createAt),
+    };
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
 // import 'firebase_options.dart'; // ini nnti kalau udh setup firebase
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
